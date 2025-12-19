381a165d15d57a840a76f26ab0fbc84355c98dee
Sasi <sasikiran.tt3011@gmail.com>
2025-12-19 18:04:52 +0530

    Update to use deployed backend and payment server URLs, fix emailService to use env vars

--- frontend/src/utils/emailService.js
+++ frontend/src/utils/emailService.js
@@ -1,4 +1,6 @@
-const API_URL = 'http://localhost:5000/api/email';
+const API_URL = import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL}/email` 
+    : 'http://localhost:5000/api/email';
 
 export const sendStudentCredentials = async (data) => {
     try {
